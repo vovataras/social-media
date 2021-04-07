8fe0df2be8cc5a5dd63c3ad33e633bdc61139948
Volodya Taras <proger.sdk@gmail.com>
2021-04-07 22:49:52 +0300

    project configuration

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '@styles/globals.scss'
+
+import { AppProps } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ src/typings/modules.d.ts
@@ -0,0 +1,25 @@
+//* styles
+declare module '*.css'
+declare module '*.scss'
+
+//* images
+declare module '*.svg'
+declare module '*.svg?tag'
+declare module '*.svg?file'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
+
+//* videos
+declare module '*.mp4'
+
+//* fonts
+declare module '*.eot'
+declare module '*.woff'
+declare module '*.woff2'
+
+//* data
+declare module '*.json'
